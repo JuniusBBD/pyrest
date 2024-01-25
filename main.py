4a3204820883a48c66a398588f2d7f59e4e95f35
Junius Lekgwara <junius.lekgwara@vcontractor.co.za>
2024-01-25 14:44:20 +0200

    added render yml

--- main.py
+++ main.py
@@ -7,36 +7,40 @@ import os
 
 app = FastAPI()
 
-# Define EasyOCR reader
-reader = easyocr.Reader(['en'])
+@app.get('/')
+async def home():
+  return 'Hello World'
 
-class ExtractedTextResponse:
-    def __init__(self, text):
-        self.text = text
+# # Define EasyOCR reader
+# reader = easyocr.Reader(['en'])
 
-@app.post("/extract_text")
-async def extract_text_from_image(file: UploadFile = File(...)):
-    # Ensure the uploaded file is an image
-    if not file.content_type.startswith("image"):
-        raise HTTPException(status_code=400, detail="File must be an image")
+# class ExtractedTextResponse:
+#     def __init__(self, text):
+#         self.text = text
 
-    # Create a temporary file to save the uploaded image
-    with tempfile.NamedTemporaryFile(delete=False) as temp_image:
-        temp_image.write(await file.read())
-        temp_image_path = temp_image.name
+# @app.post("/extract_text")
+# async def extract_text_from_image(file: UploadFile = File(...)):
+#     # Ensure the uploaded file is an image
+#     if not file.content_type.startswith("image"):
+#         raise HTTPException(status_code=400, detail="File must be an image")
 
-    try:
-        # Read the image using EasyOCR by providing the file path
-        result = reader.readtext(temp_image_path)
+#     # Create a temporary file to save the uploaded image
+#     with tempfile.NamedTemporaryFile(delete=False) as temp_image:
+#         temp_image.write(await file.read())
+#         temp_image_path = temp_image.name
 
-        # Extract text from the EasyOCR result
-        extracted_text = ' '.join([text[1] for text in result])
+#     try:
+#         # Read the image using EasyOCR by providing the file path
+#         result = reader.readtext(temp_image_path)
 
-        # Create a response object
-        response = ExtractedTextResponse(text=extracted_text)
+#         # Extract text from the EasyOCR result
+#         extracted_text = ' '.join([text[1] for text in result])
 
-        return JSONResponse(content=response.__dict__)
+#         # Create a response object
+#         response = ExtractedTextResponse(text=extracted_text)
 
-    finally:
-        # Cleanup: Delete the temporary file
-        os.remove(temp_image_path)
+#         return JSONResponse(content=response.__dict__)
+
+#     finally:
+#         # Cleanup: Delete the temporary file
+#         os.remove(temp_image_path)
