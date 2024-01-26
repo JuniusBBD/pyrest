c0af25f7011f8ddc10b4e27344b8ed56845cdc98
Junius Lekgwara <junius.lekgwara@vcontractor.co.za>
2024-01-26 15:12:47 +0200

    updated docker file

--- main.py
+++ main.py
@@ -11,36 +11,36 @@ app = FastAPI()
 async def home():
   return 'Hello World'
 
-# # Define EasyOCR reader
-# reader = easyocr.Reader(['en'])
+# Define EasyOCR reader
+reader = easyocr.Reader(['en'])
 
-# class ExtractedTextResponse:
-#     def __init__(self, text):
-#         self.text = text
+class ExtractedTextResponse:
+    def __init__(self, text):
+        self.text = text
 
-# @app.post("/extract_text")
-# async def extract_text_from_image(file: UploadFile = File(...)):
-#     # Ensure the uploaded file is an image
-#     if not file.content_type.startswith("image"):
-#         raise HTTPException(status_code=400, detail="File must be an image")
+@app.post("/extract_text")
+async def extract_text_from_image(file: UploadFile = File(...)):
+    # Ensure the uploaded file is an image
+    if not file.content_type.startswith("image"):
+        raise HTTPException(status_code=400, detail="File must be an image")
 
-#     # Create a temporary file to save the uploaded image
-#     with tempfile.NamedTemporaryFile(delete=False) as temp_image:
-#         temp_image.write(await file.read())
-#         temp_image_path = temp_image.name
+    # Create a temporary file to save the uploaded image
+    with tempfile.NamedTemporaryFile(delete=False) as temp_image:
+        temp_image.write(await file.read())
+        temp_image_path = temp_image.name
 
-#     try:
-#         # Read the image using EasyOCR by providing the file path
-#         result = reader.readtext(temp_image_path)
+    try:
+        # Read the image using EasyOCR by providing the file path
+        result = reader.readtext(temp_image_path)
 
-#         # Extract text from the EasyOCR result
-#         extracted_text = ' '.join([text[1] for text in result])
+        # Extract text from the EasyOCR result
+        extracted_text = ' '.join([text[1] for text in result])
 
-#         # Create a response object
-#         response = ExtractedTextResponse(text=extracted_text)
+        # Create a response object
+        response = ExtractedTextResponse(text=extracted_text)
 
-#         return JSONResponse(content=response.__dict__)
+        return JSONResponse(content=response.__dict__)
 
-#     finally:
-#         # Cleanup: Delete the temporary file
-#         os.remove(temp_image_path)
+    finally:
+        # Cleanup: Delete the temporary file
+        os.remove(temp_image_path)
