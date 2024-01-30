f09cd44dfe8beb01ad9897bc06270410e979ff03
Junius Lekgwara <junius.lekgwara@vcontractor.co.za>
2024-01-30 11:47:36 +0200

    updated docker file

--- Dockerfile
+++ Dockerfile
@@ -15,9 +15,10 @@ COPY . /app
 
 # Install any needed packages specified in requirements.txt
 RUN pip install --no-cache-dir -r /app/requirements.txt
+RUN pip install python-multipart
 
 # Make port 80 available to the world outside this container
 EXPOSE 80
 
 # Run app.py when the container launches
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
+CMD ["uvicorn", "main:app", "--host", "::", "--port", "80"]
--- main.py
+++ main.py
@@ -1,46 +1,35 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import easyocr
+from PIL import Image
 import io
-import tempfile
-import os
 
 app = FastAPI()
 
-@app.get('/')
-async def home():
-  return 'Hello World'
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# Define EasyOCR reader
 reader = easyocr.Reader(['en'])
 
-class ExtractedTextResponse:
-    def __init__(self, text):
-        self.text = text
-
 @app.post("/extract_text")
-async def extract_text_from_image(file: UploadFile = File(...)):
-    # Ensure the uploaded file is an image
-    if not file.content_type.startswith("image"):
-        raise HTTPException(status_code=400, detail="File must be an image")
-
-    # Create a temporary file to save the uploaded image
-    with tempfile.NamedTemporaryFile(delete=False) as temp_image:
-        temp_image.write(await file.read())
-        temp_image_path = temp_image.name
-
+async def extract_text(file: UploadFile = File(...)):
     try:
-        # Read the image using EasyOCR by providing the file path
-        result = reader.readtext(temp_image_path)
+        contents = await file.read()
+        image = Image.open(io.BytesIO(contents))
+        result = reader.readtext(image)
 
-        # Extract text from the EasyOCR result
-        extracted_text = ' '.join([text[1] for text in result])
+        extracted_text = ""
+        for detection in result:
+            extracted_text += detection[1] + "\n"
 
-        # Create a response object
-        response = ExtractedTextResponse(text=extracted_text)
+        return JSONResponse(content={"extracted_text": extracted_text}, status_code=200)
 
-        return JSONResponse(content=response.__dict__)
-
-    finally:
-        # Cleanup: Delete the temporary file
-        os.remove(temp_image_path)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
